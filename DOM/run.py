cc4121681c2f49582f570532fca7fed7a5d7bd6d
AlexDev-py <aleks.filiov@yandex.ru>
2022-01-30 17:18:27 +0900

    change HOST

--- DOM/run.py
+++ DOM/run.py
@@ -39,8 +39,8 @@ os.environ["LOCATIONS_PATH"] = os.path.join(os.environ["APP_DIR"], "locations")
 # Версия приложения
 os.environ["VERSION"] = "0.0.0"
 # Сервер
-os.environ["HOST"] = "http://127.0.0.1:5000"  # localhost
-# os.environ["HOST"] = "https://dungeon-of-masters.herokuapp.com"
+# os.environ["HOST"] = "http://127.0.0.1:5000"  # localhost
+os.environ["HOST"] = "https://dungeon-of-masters.herokuapp.com"
 
 pygame.init()
 
