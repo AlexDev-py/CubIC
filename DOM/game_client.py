07cfc66a71e4f7c676f519d0d4b10c6abbf95adf
AlexDev-py <aleks.filiov@yandex.ru>
2022-02-03 13:20:17 +0900

    new - Начало меню покупки предметов

--- DOM/game_client.py
+++ DOM/game_client.py
@@ -11,7 +11,7 @@ import typing as ty
 
 import pygame as pg
 
-from base import WidgetsGroup, Group, Label, Alert, Button, Anchor, Line
+from base import WidgetsGroup, Group, Label, Alert, Button, Anchor, Line, Text
 from database.field_types import Resolution
 from settings_alert import Settings
 from utils import load_image, FinishStatus, InfoAlert
@@ -20,6 +20,7 @@ if ty.TYPE_CHECKING:
     from network import NetworkClient
     from game.player import Player
     from game.item import Item
+    from base.types import CordFunction
 
 
 class EscMenu(Alert):
@@ -218,7 +219,7 @@ class Field(WidgetsGroup):
 
 
 class StatWidget(WidgetsGroup):
-    def __init__(self, name: str, x: int, y: int, icon: str, value: int):
+    def __init__(self, name: str, x: int, y: int, icon: str, value: str):
         icon_size = int(int(os.environ["icon_size"]) * 0.5)
         font = os.environ.get("font")
 
@@ -243,7 +244,7 @@ class StatWidget(WidgetsGroup):
             f"{self.name}-ValueLabel",
             x=self.icon.rect.right + 5,
             y=lambda obj: self.icon.height / 2 - obj.rect.height / 2,
-            text=str(value),
+            text=value,
             color=pg.Color("red"),
             font=pg.font.Font(font, icon_size),
         )
@@ -286,7 +287,7 @@ class StatsWidget(WidgetsGroup):
                 x = 0
                 y += self.stats[-1].rect.height
 
-        widget = StatWidget(f"{self.name}-{icon}-StatWidget", x, y, icon, value)
+        widget = StatWidget(f"{self.name}-{icon}-StatWidget", x, y, icon, str(value))
         self.stats.append(widget)
         return widget
 
@@ -476,7 +477,7 @@ class PlayersMenu(WidgetsGroup):
 
 class ItemStand(WidgetsGroup):
     def __init__(self, name: str, x: int, y: int, width: int, item: Item):
-        icon_size = int(int(os.environ["icon_size"]))
+        icon_size = int(os.environ["icon_size"])
         self.item = item
 
         super(ItemStand, self).__init__(None, "name", x=x, y=y, width=width)
@@ -496,9 +497,9 @@ class ItemStand(WidgetsGroup):
             ),
         )
 
-        self.item = Label(
+        self.item_icon = Label(
             self,
-            f"{name}-ItemLabel",
+            f"{name}-ItemIconLabel",
             x=lambda obj: round(self.rect.width / 2 - obj.rect.width / 2),
             y=0,
             width=icon_size,
@@ -511,8 +512,78 @@ class ItemStand(WidgetsGroup):
         )
 
 
+class ItemDescription(WidgetsGroup):
+    def __init__(
+        self,
+        parent: WidgetsGroup,
+        name: str,
+        x: int | CordFunction,
+        y: int | CordFunction,
+        item: Item,
+        item_index: int,
+    ):
+        font_size = int(os.environ["font_size"])
+        icon_size = int(os.environ["icon_size"])
+        font = os.environ.get("font")
+
+        self.item = item
+        self.item_index = item_index
+
+        super(ItemDescription, self).__init__(parent, name, x=x, y=y)
+
+        self.icon = Label(
+            self,
+            f"{self.name}-IconLabel",
+            x=0,
+            y=0,
+            width=icon_size,
+            height=icon_size,
+            sprite=load_image(
+                item.icon,
+                namespace=os.environ["ITEMS_PATH"],
+                size=(icon_size, icon_size),
+            ),
+        )
+
+        self.name = Text(
+            self,
+            f"{self.name}-NameLabel",
+            x=self.icon.rect.right + 5,
+            y=lambda obj: self.icon.rect.height / 2 - obj.rect.height / 2,
+            width=parent.rect.width - parent.padding * 2 - icon_size - 5,
+            text=item.name,
+            color=pg.Color("red"),
+            font=pg.font.Font(font, font_size),
+            soft_split=True,
+        )
+
+        self.stats: list[StatWidget] = []
+
+        for stat_name, stat_value in self.item.desc.items():
+            self.add_stat(f"{stat_name}.png", stat_value)
+
+        self.add(*self.stats)
+
+    def add_stat(self, icon: str, value: str) -> StatWidget:
+        if not len(self.stats):
+            x = 0
+            y = self.icon.rect.bottom + 5
+        else:
+            y = self.stats[-1].rect.y
+            x = self.stats[-1].rect.right
+            if x + self.stats[-1].rect.width > self.rect.width:
+                x = 0
+                y += self.stats[-1].rect.height
+
+        widget = StatWidget(f"{self.name}-{icon}-StatWidget", x, y, icon, value)
+        self.stats.append(widget)
+        return widget
+
+
 class ShopMenu(WidgetsGroup):
     def __init__(self, parent: GameClientScreen):
+        font_size = int(os.environ["font_size"])
+        font = os.environ.get("font")
         resolution = Resolution.converter(os.environ["resolution"])
 
         self.network_client = parent.network_client
@@ -527,11 +598,43 @@ class ShopMenu(WidgetsGroup):
             padding=5,
         )
 
-        self.items = []
+        self.items: list[ItemStand] = []
         for item in self.network_client.room.shop:
             self.add_item(item)
 
         self.add(*self.items)
+
+        self.item_preview = WidgetsGroup(
+            self,
+            f"{self.name}-ItemPreview",
+            x=0,
+            y=lambda obj: round(self.rect.bottom - obj.rect.height),
+            width=self.width - self.padding * 2,
+            padding=10,
+            hidden=True,
+        )
+        self.item_preview.disable()
+
+        self.item_desc: ItemDescription = ...
+
+        self.buy_button = Button(
+            self.item_preview,
+            name=f"{self.item_preview.name}-BuyButton",
+            x=0,
+            y=lambda obj: (self.item_desc.rect.bottom + 10)
+            if self.item_desc is not ...
+            else 0,
+            width=self.item_preview.rect.width - self.item_preview.padding * 2,
+            text="Купить",
+            padding=5,
+            color=pg.Color("red"),
+            active_background=pg.Color("gray"),
+            font=pg.font.Font(font, font_size),
+            anchor=Anchor.center,
+            border_color=pg.Color("red"),
+            border_width=2,
+            callback=lambda event: ...,
+        )
 
     def add_item(self, item: Item | None) -> ItemStand:
         icon_size = int(int(os.environ["icon_size"]))
@@ -558,6 +661,33 @@ class ShopMenu(WidgetsGroup):
         )
         self.items.append(widget)
         return widget
+
+    def handle_event(self, event: pg.event.Event) -> None:
+        super(ShopMenu, self).handle_event(event)
+        if self.enabled:
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == pg.BUTTON_LEFT:
+                    if self.get_global_rect().collidepoint(event.pos):
+                        for i, item in enumerate(self.items):
+                            if item.get_global_rect().collidepoint(event.pos):
+                                if item.item:
+                                    if self.item_desc is not ...:
+                                        self.item_preview.remove(self.item_desc)
+                                    self.item_desc = ItemDescription(
+                                        self.item_preview,
+                                        f"{self.item_preview.name}-{i}-ItemDescription",
+                                        x=0,
+                                        y=0,
+                                        item=item.item,
+                                        item_index=i,
+                                    )
+                                    self.item_preview.enable()
+                                    self.item_preview.show()
+                                else:
+                                    self.item_preview.disable()
+                                    self.item_preview.hide()
+                                self.item_preview.update()
+                                break
 
 
 class GameClientScreen(Group):
