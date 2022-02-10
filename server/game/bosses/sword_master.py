94d9edd59885798b8c425a92f07bb210a3bddec6
Sanzerin <StrayGid@yandex.ru>
2022-02-09 20:20:40 -0400

    modified: sword master add shield master

--- server/game/bosses/sword_master.py
+++ server/game/bosses/sword_master.py
@@ -35,16 +35,44 @@ class SwordMaster(BaseBoss):
         cords = get_rect(self.pos, radius=2, field_size=len(room.field))
         self.hit(cords, players, 4)
 
-
     def third_skill(
             self, field: list[list[True | False]], players: list[Player], room: Room
     ) -> None:
         closest_player = get_closest_player(self.pos, players)
-        delta_cord = get_delta_cord(self.pos, closest_player.character.pos, delta=2)
-        cord = (self.pos[0] + delta_cord[0], self.pos[1] + delta_cord[1])
-        cords = get_all_neighboring_cords(*cord)
-        self.hit(cords, players, 6)
+        delta_cord_2 = get_delta_cord(self.pos, closest_player.character.pos, delta=2)
+        delta_cord_1 = get_delta_cord(self.pos, closest_player.character.pos, delta=1)
+        cords = []
 
+        cord_1 = (self.pos[0] + delta_cord_1[0], self.pos[1] + delta_cord_1[1])
+        cords.append(cord_1)
+        if cord_1[0] == self.pos[0] and cord_1[1] != self.pos[1]:
+            cord_top = (cord_1[0] - 1, cord_1[1])
+            cord_bottom = (cord_1[0] + 1, cord_1[1])
+            cords.extend([cord_top, cord_bottom])
+        elif cord_1[0] != self.pos[0] and cord_1[1] == self.pos[1]:
+            cord_left = (cord_1[0], cord_1[1] - 1)
+            cord_right = (cord_1[0], cord_1[1] + 1)
+            cords.extend([cord_left, cord_right])
+        else:
+            cord_first = (cord_1[0], cord_1[1] + delta_cord_1[1])
+            cord_second = (cord_1[0] + delta_cord_1[0], cord_1[1])
+            cords.extend([cord_first, cord_second])
+
+        cord_2 = (self.pos[0] + delta_cord_2[0], self.pos[1] + delta_cord_2[1])
+        cords.append(cord_2)
+        if cord_2[0] == self.pos[0] and cord_2[1] != self.pos[1]:
+            cord_top_2 = (cord_2[0] - 1, cord_2[1])
+            cord_bottom_2 = (cord_2[0] + 1, cord_2[1])
+            cords.extend([cord_top_2, cord_bottom_2])
+        elif cord_2[0] != self.pos[0] and cord_2[1] == self.pos[1]:
+            cord_left_2 = (cord_2[0], cord_2[1] - 1)
+            cord_right_2 = (cord_2[0], cord_2[1] + 1)
+            cords.extend([cord_left_2, cord_right_2])
+        else:
+            cord_first_2 = (cord_2[0], cord_2[1] + delta_cord_2[1])
+            cord_second_2 = (cord_2[0] + delta_cord_2[0], cord_2[1])
+            cords.extend([cord_first_2, cord_second_2])
+        self.hit(cords, players, 6)
 
     def fourth_skill(
             self, field: list[list[True | False]], players: list[Player], room: Room
