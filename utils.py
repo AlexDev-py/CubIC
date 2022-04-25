7932d2789c0b45ebc9ce631d21f6bed99d3a3a51
AlexDev-py <aleks.filiov@yandex.ru>
2022-04-25 20:52:14 +0900

    update - подсчет комментариев

--- utils.py
+++ utils.py
@@ -37,6 +37,7 @@ def rows_count(all_info=True):
     """
     all_files = []
     all_rows = []
+    all_rows_without_docstrings = []
     all_sizes = []
 
     def handle_file(name: str):
@@ -45,16 +46,32 @@ def rows_count(all_info=True):
 
         size = os.path.getsize(name)
         with open(name, encoding="utf-8") as f:
-            rows = len(f.readlines()) + 1
+            f_data = list(map(str.strip, f.readlines()))
+            rows = len(f_data) + 1
+
+            start_i = -1
+            while '"""' in f_data:
+                if start_i == -1:
+                    start_i = f_data.index('"""')
+                    f_data[start_i] = ""
+                    if not (start_i == 0 or f_data[start_i - 1].endswith(":")):
+                        continue
+                else:
+                    end_i = f_data.index('"""')
+                    f_data[start_i : end_i + 1] = []
+                    start_i = -1
+
+            rows_without_docstrings = len(f_data) + 1
 
         if all_info:
             logger.opt(colors=True).info(
                 f"\t<g>{name}</g>\n"
                 f"Кол-во строк: <e>{rows}</e>\n"
+                f"Кол-во строк (без комментариев): <e>{rows_without_docstrings}</e>\n"
                 f"Размер: <e>{convert(size)}</e>\n"
             )
 
-        return rows, size
+        return rows, rows_without_docstrings, size
 
     for dir_name in ["DOM", "DOM Server"]:
         for root, dirs, files in os.walk(dir_name):
@@ -64,16 +81,23 @@ def rows_count(all_info=True):
                 if res:
                     all_files.append(file_path)
                     all_rows.append(res[0])
-                    all_sizes.append(res[1])
+                    all_rows_without_docstrings.append(res[1])
+                    all_sizes.append(res[2])
 
     logger.opt(colors=True).info(
         f"\n\t<g>Итоги</g>\n"
         f"Всего файлов: <e>{len(all_files)}</e>\n"
         f"Всего строк: <e>{sum(all_rows)}</e>\n"
+        f"Всего строк (без комментариев): <e>{sum(all_rows_without_docstrings)}</e>\n"
+        f"Всего строк комментариев: <e>{sum(all_rows) - sum(all_rows_without_docstrings)}</e>\n"
         f"Общий размер: <e>{convert(sum(all_sizes))}</e>\n"
         f"Среднее кол-во строк: <e>{sum(all_rows) // len(all_rows)}</e>\n"
         f"Максимальное кол-во строк: <e>{max(all_rows)}</e> "
         f"(<g>{all_files[all_rows.index(max(all_rows))]}</g>)\n"
+        f"Максимальное кол-во строк (без комментариев): "
+        f"<e>{max(all_rows_without_docstrings)}</e> (<g>"
+        f"{all_files[all_rows_without_docstrings.index(max(all_rows_without_docstrings))]}"
+        f"</g>)\n"
     )
 
 
